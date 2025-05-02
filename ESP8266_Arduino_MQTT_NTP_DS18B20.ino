6fbd2689e011d9f6beb65ccf07700294164a3ae3
Hank Barta <hbarta@gmail.com>
2025-05-02 15:16:07 -0500

    format MQTT payload using JSON

--- ESP8266_Arduino_MQTT_NTP_DS18B20.ino
+++ ESP8266_Arduino_MQTT_NTP_DS18B20.ino
@@ -4,10 +4,11 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <PubSubClient.h>
+#include <stdio.h>
 
 #include "secrets.h"
 
-// Following code slavishly copied from 
+// Following code slavishly copied from
 // https://randomnerdtutorials.com/guide-for-ds18b20-temperature-sensor-with-arduino/
 
 /**********************************************************/
@@ -17,12 +18,12 @@
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature sensor 
+// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 /**********************************************************/
 
 /*
-  Following code slavishly copied from 
+  Following code slavishly copied from
   https://randomnerdtutorials.com/esp8266-nodemcu-date-time-ntp-client-server-arduino/
 */
 /*========================================================*/
@@ -34,59 +35,71 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org");
 static bool do_serial = false;
 
 /*
-  Following code slavishly copied from 
+  Following code slavishly copied from
   https://www.emqx.com/en/blog/esp8266-connects-to-the-public-mqtt-broker
 */
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 WiFiClient espClient;
 PubSubClient mqtt_client(espClient);
-void connectToMQTTBroker(void) {
-  while (!mqtt_client.connected()) {
-      String client_id = "esp8266-client-" + String(WiFi.macAddress());
-      if( do_serial) Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
-      if (mqtt_client.connect(client_id.c_str(), "", "")) {
-          Serial.println("Connected to MQTT broker");
-          // mqtt_client.subscribe(mqtt_topic);          // Publish message upon successful connection
-          mqtt_client.publish("ESP8266/test", "Hi MQTT I'm ESP8266 ^^");
-      } else {
-        if( do_serial)  {
-          Serial.print("Failed to connect to MQTT broker, rc=");
-          Serial.print(mqtt_client.state());
-          Serial.println(" try again in 5 seconds");
-        }
-        delay(5000);
-      }
+
+int connectToMQTTBroker_and_publish(const char *topic, const char *payload)
+{
+  String client_id = "esp8266-client-" + String(WiFi.macAddress());
+  if (do_serial)
+    Serial.printf("Connecting to MQTT Broker as %s.....\n", client_id.c_str());
+  if (mqtt_client.connect(client_id.c_str(), "", ""))
+  {
+    if (do_serial)
+      Serial.println("Connected to MQTT broker");
+    mqtt_client.publish(topic, payload);
+    mqtt_client.disconnect();
+    return 0; // success
+  }
+  else
+  {
+    if (do_serial)
+    {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+    }
+    return mqtt_client.state();
   }
 }
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-void setup() {
+void setup()
+{
   // Configure Serial for debugging
-  if( do_serial) Serial.begin(115200);
-/**********************************************************/
+  if (do_serial)
+    Serial.begin(115200);
+  /**********************************************************/
   // Start up the library
   sensors.begin();
-/**********************************************************/
+  /**********************************************************/
 
-/* Following slavishly copied from
-   https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
-// This code requires that you provide "secrets.h" with something like
+  /* Following slavishly copied from
+     https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
+  // This code requires that you provide "secrets.h" with something like
 
-const char* ssid="myssid";
-const char* password="my_password"
+  const char* ssid="myssid";
+  const char* password="my_password"
 
-   which I suggest you do *not* check into Github etc.
-*/
-/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+     which I suggest you do *not* check into Github etc.
+  */
+  /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
   WiFi.begin(ssid, password);
 
-  if( do_serial) Serial.print("Connecting");
+  if (do_serial)
+    Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    if( do_serial) Serial.print(".");
+    if (do_serial)
+      Serial.print(".");
   }
-  if( do_serial) {
+  if (do_serial)
+  {
     Serial.println();
 
     Serial.print("Connected, IP address: ");
@@ -101,33 +114,62 @@ const char* password="my_password"
   /*========================================================*/
 
   time_t now = 0;
-  while(now < 1746133868)
+  while (now < 1746133868)
   {
     timeClient.update();
     now = timeClient.getEpochTime();
-    if( do_serial) Serial.printf("NTP time: %u\n\r", now);
+    if (do_serial)
+      Serial.printf("NTP time: %llu\n\r", now);
     delay(500);
   }
 
   mqtt_client.setServer(mqtt_broker, 1883);
-  //mqtt_client.setCallback(mqttCallback);
+  // mqtt_client.setCallback(mqttCallback);
 }
 
-void loop() {
+// buffer to build MQTT payload
+#define payload_len 200
+char payload[payload_len];
+#define float_len 10
+char float_buf[float_len];
+
+void loop()
+{
   // TODO: add code to periodically update NTP time - once/hour?
-/**********************************************************/
+  /**********************************************************/
   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures(); 
-  
-  //Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  //Serial.print(sensors.getTempCByIndex(0)); 
+  sensors.requestTemperatures();
+
+  // Serial.print("Celsius temperature: ");
+  //  Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  // Serial.print(sensors.getTempCByIndex(0));
   time_t now = timeClient.getEpochTime();
-  if( do_serial) Serial.printf("Temperature at %u : %f.2 F, %f.2 C\n\r",
-      now, sensors.getTempFByIndex(0), sensors.getTempCByIndex(0));
-  connectToMQTTBroker();
-  delay(1000);
-/**********************************************************/
+  float temperature_f = sensors.getTempFByIndex(0);
+  // dtostrf( float val, minimum field width, digits after decimal, buffer );
+  //dtostrf( temperature_f, 5, 2, float_buf );
+  sprintf(payload, 
+    "{\"t\":%lld, \"temp\":%.2f, \"device\":\"DS18B20\"}",
+    now, temperature_f);
+  Serial.println(payload);
+/*
+  snprintf(payload, payload_len, 
+    "{ \"t\":%ld, \"temp\": %s, \"device\":\"DS18B20\" }",
+    now, float_buf);
 
+  if (do_serial)
+    Serial.printf("Temperature at %u (%d): %.2f F, '%s'\n\r",
+                  now, now, temperature_f, float_buf);\
 
+    // build JSON formatted payload
+    // { "t":1746193325, "temp": 70.1, "device":"DS18B20" }
+    snprintf(payload, payload_len, 
+      "{ \"t\":%ld, \"temp\": %s, \"device\":\"DS18B20\" }",
+      now, float_buf);
+    if (do_serial)
+      Serial.println(payload);
+*/
+  
+  connectToMQTTBroker_and_publish(my_topic, payload);
+  delay(10000);
+  /**********************************************************/
 }
